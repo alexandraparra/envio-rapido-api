0adcaaac6ff834afb28e4f7ef111d1c20520f5e3
Alexandra Parra <alexandraparrarz@gmail.com>
2025-11-11 13:25:45 -0300

    Estrutura do projeto

---
+++ src/main/java/com/gft/envio_rapido_api/EnvioRapidoApiApplication.java
@@ -0,0 +1,13 @@
+package com.gft.envio_rapido_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvioRapidoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvioRapidoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gft/envio_rapido_api/EnvioRapidoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gft.envio_rapido_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvioRapidoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
